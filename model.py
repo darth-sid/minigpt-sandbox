a9ee3e1aded81ccc31dafc9dcf747854f9e5c646
Siddharth Ajay <sid.ajay05@gmail.com>
2026-06-20 21:12:15 -0400

    refactor + eval metrics

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# pyright: reportPrivateImportUsage=false
 import torch
 from torch.nn.functional import cross_entropy, softmax
 
@@ -16,8 +17,21 @@ class LM(torch.nn.Module):
             inputs = torch.cat((inputs, next_token), dim=1)
         return inputs
 
+    @torch.no_grad()
+    def evaluate(model, data, get_batch, batch_size, context_size, eval_iters=200):
+        was_training = model.training
+        model.eval()
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            x, y = get_batch(data, batch_size, model._context_size)
+            _, loss = model(x, y)
+            losses[k] = loss.item()
+        if was_training:
+            model.train()
+        return losses.mean().item()
 
-class Naive(LM):
+
+class NaiveLM(LM):
     def __init__(self, vocab_size: int):
         super().__init__(1)
         self._table = torch.nn.Embedding(vocab_size, vocab_size)
@@ -28,6 +42,11 @@ class Naive(LM):
         if targets is not None:
             loss = cross_entropy(pred.view(-1, pred.shape[-1]), targets.view(-1))
         return pred, loss
+
+
+def _causal_mask(inputs):
+    _, T = inputs.shape
+    return torch.tril(torch.ones(T, T, device=inputs.device))
 
 
 class AttentionHead(torch.nn.Module):
@@ -46,7 +65,7 @@ class AttentionHead(torch.nn.Module):
         return softmax(QKT, dim=-1) @ V  # (B, T, d_head)
 
 
-class SingleHeadCausalAttention(LM):
+class SingleHeadCausalAttentionLM(LM):
     def __init__(self, vocab_size: int, d_embed: int, context_size: int):
         super().__init__(context_size)
         self._embed = torch.nn.Embedding(vocab_size, d_embed)
@@ -55,15 +74,50 @@ class SingleHeadCausalAttention(LM):
         self._unembed = torch.nn.Linear(d_embed, vocab_size)
 
     def forward(self, inputs, targets=None):  # (B, T), (B, T)
-        _, T = inputs.shape
-        mask = torch.tril(torch.ones(T, T, device=inputs.device))
-
-        positions = torch.arange(T)
+        positions = torch.arange(inputs.shape[1], device=inputs.device)
         pos_embeddings = self._pos_embed(positions)  # (B, T, d_embed)
         embeddings = self._embed(inputs)  # (B, T, d_embed)
         x = embeddings + pos_embeddings
 
-        att_out = self._attention(x, mask)  # (B, T, d_embed)
+        att_out = self._attention(x, _causal_mask(inputs))  # (B, T, d_embed)
+        pred = self._unembed(att_out)  # (B, T, vocab)
+        loss = None
+        if targets is not None:
+            loss = cross_entropy(pred.view(-1, pred.shape[-1]), targets.view(-1))
+        return pred, loss
+
+
+class MultiHeadAttention(torch.nn.Module):
+    def __init__(self, d_embed: int, n_head: int):
+        super().__init__()
+        d_head = d_embed // n_head
+        self._heads = torch.nn.ModuleList(
+            [AttentionHead(d_embed, d_head) for _ in range(n_head)]
+        )
+        self._blend = torch.nn.Linear(d_embed, d_embed)
+
+    def forward(self, inputs, mask=None):
+        return self._blend(
+            torch.cat([att(inputs, mask) for att in self._heads], dim=-1)
+        )
+
+
+class MultiHeadCausalAttentionLM(LM):
+    def __init__(self, vocab_size: int, d_embed: int, n_head: int, context_size: int):
+        super().__init__(context_size)
+        self._embed = torch.nn.Embedding(vocab_size, d_embed)
+        self._pos_embed = torch.nn.Embedding(context_size, d_embed)
+        self._attention = MultiHeadAttention(d_embed, n_head)
+        self._unembed = torch.nn.Linear(d_embed, vocab_size)
+
+    def forward(self, inputs, targets=None):
+        positions = torch.arange(inputs.shape[1], device=inputs.device)
+        pos_embeddings = self._pos_embed(positions)  # (B, T, d_embed)
+        embeddings = self._embed(inputs)  # (B, T, d_embed)
+        x = embeddings + pos_embeddings
+
+        mask = _causal_mask(inputs)
+        att_out = self._attention(x, mask)
         pred = self._unembed(att_out)  # (B, T, vocab)
         loss = None
         if targets is not None:
--- train.py
+++ train.py
@@ -1,14 +1,15 @@
+# pyright: reportPrivateImportUsage=false
 from data import load_data, get_batch, decode
-from model import Naive, SingleHeadCausalAttention
+from model import MultiHeadCausalAttentionLM, NaiveLM, SingleHeadCausalAttentionLM
 import torch
 from tqdm import tqdm, trange
 
 
-def train_model(data, model, iters, batch_size, block_size, lr, print_freq=None):
+def train_model(train, test, model, iters, batch_size, block_size, lr, print_freq=None):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     for i in trange(iters):
-        x, y = get_batch(data, batch_size, block_size)
+        x, y = get_batch(train, batch_size, block_size)
         _, loss = model(x, y)
         optimizer.zero_grad()
         loss.backward()
@@ -16,12 +17,20 @@ def train_model(data, model, iters, batch_size, block_size, lr, print_freq=None)
 
         if print_freq is not None and i % print_freq == 0:
             tqdm.write(str(loss.item()))
+    for name, data in [("train", train), ("test", test)]:
+        print(
+            f"{name}:{model.evaluate(data, get_batch, batch_size, block_size, block_size)}"
+        )
 
 
 train, test, vocab = load_data("input.txt")
 
-models = [Naive(len(vocab)), SingleHeadCausalAttention(len(vocab), 32, 8)]
+models = [
+    NaiveLM(len(vocab)),
+    SingleHeadCausalAttentionLM(len(vocab), 32, 8),
+    MultiHeadCausalAttentionLM(len(vocab), 32, 4, 8),
+]
 
 for m in models:
-    train_model(train, m, 10000, 32, 8, 1e-2, print_freq=500)
+    train_model(train, test, m, 10000, 32, 8, 1e-2, print_freq=500)
     print(decode(m.predict(torch.zeros((1, 1), dtype=torch.long))[0].tolist(), vocab))
